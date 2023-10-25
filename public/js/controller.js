048ed34d8ca771e9af3a0d8324759d6d50ca417a
Emma <ecardosa@cendrassos.net>
2023-10-25 15:49:16 +0200

    semana3 dia3

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,16 +1,25 @@
-function get(variable) {
-    var jsonData = new Object();
-    
-    // Use $.getJSON to fetch data from "get.php" and pass the variable as a query parameter
-    $.getJSON('get.php?lang=' + variable, function(data) {
-        console.log(variable);
-        // Your code to handle the JSON data
-        jsonData = data;
-        console.log(jsonData);
-    });
-}
 
-document.getElementById('getDataButton').onclick = function() {
-    // Call the get() function when the button is clicked
-    get('apartament'); // Replace 'your_variable_here' with the actual variable you want to pass
-};
+
+document.addEventListener('DOMContentLoaded', function () {
+    const startDateInput = document.getElementById('start');
+    const endDateInput = document.getElementById('end');
+
+    // Escuchar cambios en las fechas
+    startDateInput.addEventListener('change', () => {
+        const startDate = new Date(startDateInput.value);
+        const endDate = new Date(endDateInput.value);
+
+        if (startDate > endDate) {
+            endDateInput.value = startDateInput.value;
+        }
+    });
+
+    endDateInput.addEventListener('change', () => {
+        const startDate = new Date(startDateInput.value);
+        const endDate = new Date(endDateInput.value);
+
+        if (endDate < startDate) {
+            startDateInput.value = endDateInput.value;
+        }
+    });
+});
