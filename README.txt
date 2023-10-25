d14b5dbba22213db8ee35e255beb16202f8ddddf
Victor <vgonzalez@cendrassos.net>
2023-10-25 17:35:16 +0200

    DELETE

--- README.txt
+++
@@ -1,1 +0,0 @@
-Aquest projecte és un treball conjunt entre Alejandro Espinoza i Víctor González per al curs de 2n de DAW a l'Institut Cendrassos
--- src/controllers/ctrlRegister.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Este controlador sirve para ver la pagina de register.php
-
-function ctrlRegistroView($request,  $response,$container){
-    $response->setTemplate("register.php");
-    return $response;
-}
