6af5d04cfc2a5eeac7d36f77fbd9e89d91c7a5c1
Aespinozacortez <aespinozacortez04@gmail.com>
2023-10-27 16:08:57 +0200

    Add_Apartament

---
+++ src/controllers/ctrlDoApartament.php
@@ -0,0 +1,27 @@
+<?php
+// Este controlador transforma los datos a variables php y llama a la función ctrlRegister
+// de Users.php
+
+function ctrlDoApartament($request, $response, $container){
+
+    $title = $request->get(INPUT_POST, "title");
+    $postal = $request->get(INPUT_POST, "postal");
+    $descripcion = $request->get(INPUT_POST, "descripcion");
+    $metros = $request->get(INPUT_POST, "metros");
+    $habitaciones = $request->get(INPUT_POST, "habitaciones");
+    $TBaja = $request->get(INPUT_POST, "TBaja");
+    $TALT = $request->get(INPUT_POST, "TALT");
+    $cancelacion = $request->get(INPUT_POST, "cancelacion");
+
+    $userModel = $container->Users();
+
+    $userModel = $userModel->addapartament($title, $postal, $descripcion, $metros, $habitaciones, $TBaja, $TALT, $cancelacion);
+    if ($userModel) {        
+        $response->redirect("location: index.php");
+    } else {
+        $response->redirect("location: index.php");
+    }
+
+    return $response;
+}
+?>
--- src/views/index.php
+++ src/views/index.php
@@ -110,6 +110,13 @@
       </form>
     </div>
 
+    <div class="bottomapartament">
+      <form action="index.php" method="post">
+          <input type="hidden" name="r" value="apartament">
+          <button type="submit" class="btn btn-primary">Añadir apartamento</button>
+      </form>
+    </div>
+
 
     <script src="app.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> 
